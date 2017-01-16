7a07fc228e7608d81e97a13e5d5788afd4f640bb
SurajKoshti <kostisuraj95@gmail.com>
2017-01-16 18:23:33 +0530

    Update difference_of_squares.rb

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -3,12 +3,12 @@ class Squares
     @number = number
   end
   def square_of_sum
-    return ((@number*(@number + 1))/2)**2
+    ((@number*(@number + 1))/2)**2
   end
   def sum_of_squares
-    return (@number*(@number + 1)*(2*@number + 1))/6
+    (@number*(@number + 1)*(2*@number + 1))/6
   end
   def difference
-    return square_of_sum - sum_of_squares
+    square_of_sum - sum_of_squares
   end
 end
